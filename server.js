f4bed82ae421b498ea46d6511c78415589d2c9c6
Khang <55619201+khang-nd@users.noreply.github.com>
2021-05-14 07:26:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -39,3 +39,5 @@ app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () {
   console.log('Proxy server listening on port ' + app.get('port'));
 });
+
+module.exports = app;
